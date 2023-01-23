106f85f140f544029c0bab173430202fc81a084c
Azeza Alsayed <aza@DESKTOP-78JGSPR.mynetworksettings.com>
2023-01-23 12:54:16 -0500

    edited

--- src/test/java/com/noorteck/qa/runners/Runne3.java
+++
@@ -1,5 +0,0 @@
-package com.noorteck.qa.runners;
-
-public class Runne3 {
-
-}
--- src/test/java/com/noorteck/qa/runners/Runner3.java
+++ src/test/java/com/noorteck/qa/runners/Runner3.java
@@ -14,6 +14,6 @@ import io.cucumber.junit.CucumberOptions;
 									"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
 					monochrome = true
 						)
-public class Runner2 {
+public class Runner3 {
 
 }
